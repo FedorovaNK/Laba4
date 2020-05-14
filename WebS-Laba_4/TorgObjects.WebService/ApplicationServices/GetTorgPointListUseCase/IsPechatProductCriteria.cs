200d8070f307f4186f374026f4ee5e7e4f348579
FedorovaNK <64545483+FedorovaNK@users.noreply.github.com>
2020-05-15 01:21:54 +0300

    Add files via upload

---
+++ WebS-Laba_4/TorgObjects.WebService/ApplicationServices/GetTorgPointListUseCase/IsPechatProductCriteria.cs
@@ -0,0 +1,20 @@
+﻿using TorgObjects.DomainObjects;
+using TorgObjects.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace TorgObjects.ApplicationServices.GetMedPointListUseCase
+{
+    public class IsPechatProductCriteria : ICriteria<MedPoint>
+    {
+        public string IsPechatProduct { get; }
+
+        public IsPechatProductCriteria(string isPechatProduct)
+            => IsPechatProduct = isPechatProduct;
+
+        public Expression<Func<MedPoint, bool>> Filter
+            => (mp => mp.IsPechatProduct == IsPechatProduct);
+    }
+}
---
+++ WebS-Laba_4/TorgObjects.WebService/InfrastructureServices/Controllers/TorgPointsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TorgObjects.DomainObjects;
+using TorgObjects.ApplicationServices.GetTorgPointListUseCase;
+using TorgObjects.InfrastructureServices.Presenters;
+
+namespace TorgObjects.InfrastructureServices.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TorgPointsController : ControllerBase
+    {
+        private readonly ILogger<TorgPointsController> _logger;
+        private readonly IGetTorgPointListUseCase _getTorgPointListUseCase;
+
+        public TorgPointsController(ILogger<TorgPointsController> logger,
+                                IGetTorgPointListUseCase getTorgPointListUseCase)
+        {
+            _logger = logger;
+            _getTorgPointListUseCase = getTorgPointListUseCase;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllTorgPoints()
+        {
+            var presenter = new TorgPointListPresenter();
+            await _getTorgPointListUseCase.Handle(GetTorgPointListUseCaseRequest.CreateAllTorgPointsRequest(), presenter);
+            return presenter.ContentResult;
+        }
+
+        [HttpGet("{TorgpointId}")]
+        public async Task<ActionResult> GetTorgPoint(long TorgpointId)
+        {
+            var presenter = new TorgPointListPresenter();
+            await _getTorgPointListUseCase.Handle(GetTorgPointListUseCaseRequest.CreateTorgPointRequest(TorgpointId), presenter);
+            return presenter.ContentResult;
+        }
+
+        [HttpGet("isTorghelpfree/{isTorghelpfree}")]
+        public async Task<ActionResult> GetIsTorgHelpFreeTorgPoints(string isTorghelpfree)
+        {
+            var presenter = new TorgPointListPresenter();
+            await _getTorgPointListUseCase.Handle(GetTorgPointListUseCaseRequest.CreateIsTorgHelpFreeTorgPointsRequest(isTorghelpfree), presenter);
+            return presenter.ContentResult;
+        }
+    }
+}
